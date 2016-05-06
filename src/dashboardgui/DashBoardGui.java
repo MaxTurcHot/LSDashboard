c73e390e00866948408242153ad50da78025c956
Maxime Turcotte <maxime.t.turcotte@gmail.com>
2016-05-05 22:42:17 -0400

    Advancing thru gui

--- src/dashboardgui/DashBoardGui.java
+++ src/dashboardgui/DashBoardGui.java
@@ -14,22 +14,34 @@ import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import batchtool.*;
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 //import java.util.ArrayList;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventType;
 import javafx.geometry.Insets;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.SelectionMode;
 //import javafx.geometry.Rectangle2D;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 //import javafx.stage.FileChooser.ExtensionFilter;
 //import javafx.stage.Screen;
 import javafx.util.Callback;
+import javafx.util.converter.DoubleStringConverter;
+import javafx.util.converter.NumberStringConverter;
+import javax.swing.ListSelectionModel;
 
 /**
  *
@@ -40,6 +52,8 @@ public class DashBoardGui extends Application {
     @Override
     public void start(Stage primaryStage) {
         
+        Stage window;
+        
         // Initialize the Main Dash
         ObservableList<Line> MainList = FXCollections.observableArrayList();
         MainDash maindash = new MainDash(MainList);
@@ -48,37 +62,103 @@ public class DashBoardGui extends Application {
         String AvailableProcessesFile = "AvailableProcesses.txt";
         maindash.setAvailableprocesses(AvailableProcess.load(AvailableProcessesFile));
         TableView<Line> dasboardtable;
+
         
-        // Name Column
-        TableColumn<Line, Boolean> IsActiveLabel = new TableColumn("Is Active");
+        ////////////////////////////////////////
+        /////// COLUMN DEFINITION //////////////
+        ////////////////////////////////////////
+        
+        
+        // Is Active Column
+        TableColumn<Line, String> IsActiveLabel = new TableColumn("Is Active");
         IsActiveLabel.setMinWidth(100);
+        IsActiveLabel.setEditable(true);
         IsActiveLabel.setCellValueFactory(new PropertyValueFactory<>("IsActive"));
-        // Name Column
+        IsActiveLabel.setCellFactory(TextFieldTableCell.forTableColumn());
+        IsActiveLabel.setOnEditCommit(new EventHandler<CellEditEvent<Line, String>>() {
+            @Override
+            public void handle(CellEditEvent<Line, String> t) {
+                //
+                List<String> istrue = Arrays.asList("TRUE","True","true","1","on","ON","active");
+                List<String> isfalse = Arrays.asList("FALSE","False","false","0","OFF","off","inactive");
+                boolean containedtrue = istrue.contains(t.getNewValue());
+                boolean containedfalse = isfalse.contains(t.getNewValue());
+                if (containedtrue) {
+                    ((Line) t.getTableView().getItems().get(t.getTablePosition().getRow())).setIsActive("TRUE");
+                }
+                else if (containedfalse) {
+                    ((Line) t.getTableView().getItems().get(t.getTablePosition().getRow())).setIsActive("FALSE");
+                }
+                
+                //
+            }
+        }
+        );
+        // Comments Column
         TableColumn<Line, String> Comments = new TableColumn("Comments");
         Comments.setMinWidth(200);
+        Comments.setEditable(true);
         Comments.setCellValueFactory(new PropertyValueFactory<>("Comments"));
+        Comments.setCellFactory(TextFieldTableCell.forTableColumn());
+        Comments.setOnEditCommit(new EventHandler<CellEditEvent<Line, String>>() {
+            @Override
+            public void handle(CellEditEvent<Line, String> t) {
+                //
+                ((Line) t.getTableView().getItems().get(t.getTablePosition().getRow())).setComments(t.getNewValue());
+                //
+            }
+        }
+        );
         //ThreadID
         TableColumn<Line, String> ThreadID = new TableColumn("Thread ID");
         ThreadID.setMinWidth(100);
+        ThreadID.setEditable(true);
         ThreadID.setCellValueFactory(new PropertyValueFactory<>("ThreadID"));
-        // Name Column
+        ThreadID.setCellFactory(TextFieldTableCell.forTableColumn());
+        ThreadID.setOnEditCommit(new EventHandler<CellEditEvent<Line, String>>() {
+            @Override
+            public void handle(CellEditEvent<Line, String> t) {
+                //
+                ((Line) t.getTableView().getItems().get(t.getTablePosition().getRow())).setThreadID(t.getNewValue());
+                //
+            }
+        }
+        );
+        // Delay Column
         TableColumn<Line, Double> DelayLabel = new TableColumn("Delay (sec)");
         DelayLabel.setMinWidth(100);
+        DelayLabel.setEditable(true);
         DelayLabel.setCellValueFactory(new PropertyValueFactory<>("Delay"));
-        // Name Column
+        DelayLabel.setCellFactory(TextFieldTableCell.<Line, Double>forTableColumn(new DoubleStringConverter()));
+        DelayLabel.setOnEditCommit(new EventHandler<CellEditEvent<Line, Double>>() {
+            @Override
+            public void handle(CellEditEvent<Line, Double> t) {
+                //
+                //
+                //String var = t.getNewValue();
+                ((Line) t.getTableView().getItems().get(t.getTablePosition().getRow())).setDelay(t.getNewValue());
+                //
+            }
+        }
+        );
+        // Process Column
         TableColumn<Line, String> ProcessLabel = new TableColumn("Process");
         ProcessLabel.setMinWidth(200);
+        //ProcessLabel.
         ProcessLabel.setCellValueFactory(new PropertyValueFactory<>("ExLabel"));
         
         // Set Argument Column
         TableColumn ArgNum = new TableColumn<>("Argument #");
         ArgNum.setMinWidth(100);
         ArgNum.setCellValueFactory(new PropertyValueFactory<>("ArgumentNo"));
-        
+
+        //   Table view setting here
         dasboardtable = new TableView<>();
         dasboardtable.setItems(maindash.getDashprocesses());
         dasboardtable.setEditable(true);
+        dasboardtable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         dasboardtable.getColumns().addAll(IsActiveLabel, Comments, ThreadID, DelayLabel, ProcessLabel, ArgNum);
+        //dasboardtable.getColumns().addAll(IsActiveLabel, Comments, ThreadID, ProcessLabel, ArgNum);
         // All button here
         Button loadprojectbutton = new Button();
         loadprojectbutton.setText("Load Project Dash");
@@ -94,6 +174,50 @@ public class DashBoardGui extends Application {
         Button deletelinebutton = new Button();
         deletelinebutton.setText("Delete Process(es)");
         
+        addlinebutton.setOnAction(e -> {
+            
+            ObservableList<String> availablechoices = FXCollections.observableArrayList();
+            for (Line l:maindash.getAvailableprocesses()) {
+                availablechoices.add(l.getExLabel());
+            }
+            
+            GridPane sublayout = new GridPane();
+            sublayout.setVgap(5);
+            sublayout.setHgap(5);
+            Button acceptselection = new Button();
+            acceptselection.setText("ok");
+            acceptselection.setMinWidth(200);
+            
+            
+            ChoiceBox cb = new ChoiceBox();
+            cb.setItems(availablechoices);
+            cb.getSelectionModel().selectFirst();
+            sublayout.add(cb, 0 ,0);
+            sublayout.add(acceptselection, 0 ,1);
+            Scene subscene = new Scene(sublayout, 200, 60);
+            Stage subwindow = new Stage();
+            subwindow.setScene(subscene);
+            subwindow.setTitle("Select Process");
+            subwindow.show();
+            
+
+            acceptselection.setOnAction(f -> {
+                String selection = cb.getSelectionModel().getSelectedItem().toString();
+                for (Line l:maindash.getAvailableprocesses()) {
+                    if (l.getExLabel().equals(selection)) {
+                        System.out.println("found");
+                        maindash.addline(l);
+                        break;
+                    }
+                }
+                subwindow.close();
+            //refresh_table(dasboardtable);
+        });
+            //refresh_table(dasboardtable);
+        });
+        
+        
+        
         
         loadprojectbutton.setOnAction(e -> {
             FileChooser fileChooser = new FileChooser();
@@ -105,6 +229,74 @@ public class DashBoardGui extends Application {
             //refresh_table(dasboardtable);
         });
         
+        setargumentbutton.setOnAction(e -> {
+            //dasboardtable.getSelectionModel().getSelectedItems();
+            //for (Line l:dasboardtable.getSelectionModel().getSelectedItems()) {
+             //   l.print();
+            //}
+            
+            
+            // If selection is unique
+            if (dasboardtable.getSelectionModel().getSelectedItems().size() == 1) {
+
+                Line lineitem = dasboardtable.getSelectionModel().getSelectedItem();
+                // Arguments label
+                TableColumn<Argument, String> Arg1 = new TableColumn("Arguments");
+                Arg1.setMinWidth(200);
+                //Arg1.setEditable(true);
+                Arg1.setCellValueFactory(new PropertyValueFactory<>("datalabel"));
+                
+                // Arguments value
+                TableColumn<Argument, String> Argvalue = new TableColumn("Value");
+                Argvalue.setMinWidth(500);
+                Argvalue.setEditable(true);
+                Argvalue.setCellValueFactory(new PropertyValueFactory<>("data"));
+                Argvalue.setCellFactory(TextFieldTableCell.forTableColumn());
+                Argvalue.setOnEditCommit(new EventHandler<CellEditEvent<Argument, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Argument, String> t) {
+                        //
+                        ((Argument) t.getTableView().getItems().get(t.getTablePosition().getRow())).setData(t.getNewValue());
+                        //
+                    }
+                }
+                );
+
+                //   Table view setting here
+                TableView<Argument> argtable = new TableView<>();
+                argtable.setItems(lineitem.getListofArgument());
+                argtable.setEditable(true);
+                argtable.setMinHeight(100);
+                argtable.getColumns().addAll(Arg1, Argvalue);
+
+                //
+                Button ArgSelector = new Button();
+                ArgSelector.setText("Select");
+                ArgSelector.setOnAction(f -> {
+                    // Need to call specific argument selector here
+                    if (argtable.getSelectionModel().getSelectedItems().size() == 1) {
+                        Argument arg = argtable.getSelectionModel().getSelectedItem();
+                        System.out.println("Selector \"" + arg.getData());
+                    }
+                    
+                });
+
+                
+                
+                GridPane sublayout = new GridPane();
+                sublayout.setVgap(5);
+                sublayout.setHgap(5);
+                Scene subscene = new Scene(sublayout, 720, 420);
+                Stage subwindow = new Stage();
+                subwindow.setScene(subscene);
+                subwindow.setTitle("Argument Setting for " + dasboardtable.getSelectionModel().getSelectedItem().getComments());
+                sublayout.add(argtable,0 ,1, 3, 3);
+                sublayout.add(ArgSelector, 1 ,0);
+                subwindow.showAndWait();
+                
+                
+            }
+        });
         
         GridPane mainlayout = new GridPane();
         mainlayout.setPadding(new Insets(10, 10, 10, 10));
@@ -118,7 +310,7 @@ public class DashBoardGui extends Application {
         mainlayout.add(runlinebutton, 5, 0);
         mainlayout.add(deletelinebutton, 6, 0);
         mainlayout.add(dasboardtable,0 ,1, 10, 12);
-        Scene mainwindow = new Scene(mainlayout, 1200, 400);
+        Scene mainwindow = new Scene(mainlayout, 850, 400);
         
         // Maximize window size here
         //Screen screen = Screen.getPrimary();
@@ -127,10 +319,10 @@ public class DashBoardGui extends Application {
         //primaryStage.setY(bounds.getMinY());
         //primaryStage.setWidth(bounds.getWidth());
         //primaryStage.setHeight(bounds.getHeight());
-        
-        primaryStage.setTitle("Linux Structural Dashboard");
-        primaryStage.setScene(mainwindow);
-        primaryStage.show();
+        window = primaryStage;
+        window.setTitle("Linux Structural Dashboard");
+        window.setScene(mainwindow);
+        window.show();
     }
 
     /**
